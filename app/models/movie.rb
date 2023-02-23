a3a515743bde585a2ec752855ccdcfea5e079b0d
gdicera <giuseppedicera@uchicago.edu>
2023-02-23 23:48:17 +0000

    used ideas to add to .rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,8 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  belongs_to(:director, { :required => true, :class_name => "Director", :foreign_key => "director_id" })
+  has_many(:characters, { :class_name => "Character", :foreign_key => "movie_id", :dependent => :destroy })
+  has_many(:cast, { :through => :characters, :source => :actor })
+
 end
